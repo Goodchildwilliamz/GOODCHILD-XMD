3eca7e7ef779c78fbd228061ba456f68593241fe
GOODCHILD-MD <goodchildwilliamz@gmail.com>
2025-05-26 08:47:52 -0700

    Add files via upload

---
+++ plugins/ssweb-tool.js
@@ -0,0 +1,54 @@
+// code by ⿻ ⌜ DRAXEN Ai 🤎 ⌟⿻⃮͛🇹🇿𖤐
+
+const axios = require("axios");
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  pattern: "sss",
+  alias: ["ssweb"],
+  react: "💫",
+  desc: "Download screenshot of a given link.",
+  category: "other",
+  use: ".ss <link>",
+  filename: __filename,
+}, 
+async (conn, mek, m, {
+  from, l, quoted, body, isCmd, command, args, q, isGroup, sender, 
+  senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, 
+  groupMetadata, groupName, participants, isItzcp, groupAdmins, 
+  isBotAdmins, isAdmins, reply 
+}) => {
+  if (!q) {
+    return reply("Please provide a URL to capture a screenshot.");
+  }
+
+  try {
+    // created by draxen ai
+    const response = await axios.get(`https://api.davidcyriltech.my.id/ssweb?url=${q}`);
+    const screenshotUrl = response.data.screenshotUrl;
+
+    // give credit and use
+    const imageMessage = {
+      image: { url: screenshotUrl },
+      caption: "*WEB SS DOWNLOADER*\n\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ GOODCHILD XMD 🤎*",
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '0029Vb4vbKF6xCSMEFopxc10@newsletter',
+          newsletterName: "GOODCHILD XMD 🤎",
+          serverMessageId: 143,
+        },
+      },
+    };
+
+    await conn.sendMessage(from, imageMessage, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    reply("Failed to capture the screenshot. Please try again.");
+  }
+});
+
+// ⿻ ⌜ GOODCHILD XMD 🤎 ⌟⿻⃮͛🇹🇿𖤐
